3fafbdce227cdf314cdbc089ca5b7bdea795d127
JUGGUM <gayoung801@naver.com>
2024-11-07 09:16:05 +0900

    Initial commit

---
+++ Untitled_BE/src/main/java/com/backend/Untitled_BE/UntitledBeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Untitled_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntitledBeApplication.class, args);
+	}
+
+}
---
+++ Untitled_BE/src/test/java/com/backend/Untitled_BE/UntitledBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Untitled_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
